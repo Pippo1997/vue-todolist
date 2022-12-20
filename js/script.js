8f7b48d89089e8395191926f8c69b76f81adcf33
Filippo Sbaiz <sbaizfilippo@gmail.com>
2022-12-20 17:42:11 +0100

    push + add

--- js/script.js
+++ js/script.js
@@ -3,9 +3,53 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        
+        newItem: ``,
+        toDolist: [
+          {
+            text: `Jujutsu Kaisen`,
+            done: false,
+          },
+          {
+            text: `Naruto`,
+            done: true,
+          },
+          {
+            text: `Death Note`,
+            done: false,
+          },
+          {
+            text: `My Hero Academia`,
+            done: false,
+          },
+          {
+            text: `One Pice`,
+            done: true,
+          },
+          {
+            text: `One Punch`,
+            done: false,
+          },
+        ]
       }
-    }
+    },
+    methods: {
+      check(index){
+        let singleItem = this.toDolist[index]
+        if(singleItem.done == false){
+          singleItem.done = true
+        } else{
+          singleItem.done = false
+        }
+      },
+      addNewitem(){
+          let object = {
+          text: this.newItem,
+          done: false,
+        }
+        this.toDolist.push(object)
+        this.newitem = ``
+      }
+    },
   }).mount('#app')
 
 
